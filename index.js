48450719e4fe47b5b0e198750ebc3e8e7fd6cc8c
Satyam Lal <satyamlal@users.noreply.github.com>
2025-03-29 22:43:07 +0530

    Zod authentication set

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 const port = 3000;
@@ -11,11 +12,30 @@ app.get("/", (req, res) => {
 
 app.post("/todo", (req, res) => {
   res.send("Todo route");
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "You inputs are not correct!!!",
+    });
+    return;
+  }
 });
 
 app.get("/todos", (req, res) => {});
 
-app.put("/completed", (req, res) => {});
+app.put("/completed", (req, res) => {
+  const updatePayload = req.body;
+  const parsePayload = updateTodo.safeParse(updatePayload);
+
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "You sent wrong inputs!!!",
+    });
+    return;
+  }
+});
 
 app.listen(port, () => {
   console.log("Listening to port 3000");

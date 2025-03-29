2b7e27c5fd9b6f41a7e3f5822bd3bf66d044abcf
Satyam Lal <satyamlal@users.noreply.github.com>
2025-03-29 23:00:41 +0530

    MongoDB schema done

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://ssatyamlal:K5TRIZZxYIiybM3C@cluster-1-todo-app.dd1l071.mongodb.net/"
+);
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+
+const todo = mongoose.model("todos", todoSchema);
+
+module.exports = {
+  todo: todo,
+};
